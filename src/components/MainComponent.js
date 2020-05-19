d4a6285845996dae0009ef83f69282a728638762
Ritik Agarwal <ritikagarwal0123@gmail.com>
2020-05-20 03:47:49 +0530

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,12 @@
 import React ,{Component} from 'react';
-import {Navbar,NavbarBrand} from 'reactstrap';
+import Home from './HomeComponent';
 import Menu from './Menucomponents';
 import {DISHES} from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect} from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -10,27 +14,25 @@ class Main extends Component {
     super(props);
 
     this.state ={
-      dishes:DISHES,
-      selectedDish:null
+      dishes:DISHES
     };
   }
-  onDishSelect(dishID){
-    this.setState({
-        selectedDish: dishID
-    });
-}
+  
   render(){
+    const HomePage=() => {
+      return (
+        <Home />
+      )
+    }
   return (
     <div>
-      <Navbar dark color="primary">
-      <div className='container'>
-      <NavbarBrand href="/">Restuarant</NavbarBrand>
-      </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} 
-      onClick={(dishID)=> this.onDishSelect(dishID)}/>
-      <DishDetail 
-        dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+      <Header />
+      <Switch>
+        <Route path="/home" component={HomePage} />
+        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
+        <Redirect to="/home" />
+      </Switch>
+      <Footer />
     </div>
   );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -15,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
