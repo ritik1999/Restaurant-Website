d3f7e0e0786f5d177efe9425500ab2b2af100f24
Ritik Agarwal <ritikagarwal0123@gmail.com>
2020-06-16 08:43:17 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,7 @@ import {Card,CardImg,Breadcrumb,BreadcrumbItem,
     ,Modal,ModalBody,ModalHeader,Label,Col,Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {LocalForm,Control,Errors} from 'react-redux-form';
-
+import {Loading} from './LoadingComponent';
     function RenderComments({comments,addComment,dishId})
     {
         if(comments==null)
@@ -62,35 +62,55 @@ import {LocalForm,Control,Errors} from 'react-redux-form';
     }
 
     const DishDetail=(props) => {
-        const dish=props.dish;
-        if(dish==null)
+        if(props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if(props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if(props.dish==null)
         {
             return(<div></div>)
         }
-    return(
-        <div className="container">
-            <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem>
-                            <Link to='/menu'>Menu</Link>
-                        </BreadcrumbItem>
-                        <BreadcrumbItem active>
-                            {props.dish.name}
-                        </BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr/>
+        else if(props.dish!=null)
+        {
+                return(
+                    <div className="container">
+                        <div className="row">
+                                <Breadcrumb>
+                                    <BreadcrumbItem>
+                                        <Link to='/menu'>Menu</Link>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbItem active>
+                                        {props.dish.name}
+                                    </BreadcrumbItem>
+                                </Breadcrumb>
+                                <div className="col-12">
+                                    <h3>{props.dish.name}</h3>
+                                    <hr/>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <RenderDish dish={props.dish} />
+                                <RenderComments comments={props.comments}
+                                addComment={props.addComment}
+                                dishId={props.dish.id} />   
+                            </div>
                     </div>
-                </div>
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments}
-                    addComment={props.addComment}
-                    dishId={props.dish.id} />   
-                </div>
-        </div>
-    )
+                )
+        }
     }
     const required = (val) => val && val.length;
     const maxlength = (len) => (val) => !(val) || (val.length <= len);
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import {DISHES} from '../shared/dishes';
 
 
 
@@ -12,4 +13,25 @@ export const addComment = (dishId,rating,author,comment)=>
                 author: author,
                 comment: comment
             }
+});
+//fetchDishes is a thunk
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    } ,2000);
+}
+
+export const dishesLoading = () => ({
+    type:ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type:ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type:ActionTypes.ADD_DISHES,
+    payload : dishes
 });
